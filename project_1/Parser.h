f3a2b365193f3a5d6072a32c4f1dde95438968c4
Istiak Rahman <87246045+istiak-rahman@users.noreply.github.com>
2024-02-01 14:17:42 -0500

    changes

--- project_1/Parser.h
+++ project_1/Parser.h
@@ -7,5 +7,7 @@
 #include "Registers.h"
 
 void loadInstructions(Instruction_Memory *i_mem, const char *trace);
-void parseRType(char *opr, Instruction *instr);
+void parseRType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int funct7_IN);
+void parseIType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int Imm_IN);
+void parseSBType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int Imm_IN);
 int regIndex(char *reg);
