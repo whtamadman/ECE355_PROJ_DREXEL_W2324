3f95e7326e4d9cc63d41b6d05f46ac93aa46ade2
Istiak Rahman <87246045+istiak-rahman@users.noreply.github.com>
2024-02-01 22:51:51 -0500

    added sb type

--- project_1/Parser.h
+++ project_1/Parser.h
@@ -9,5 +9,6 @@
 void loadInstructions(Instruction_Memory *i_mem, const char *trace);
 void parseRType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int funct7_IN);
 void parseIType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int Imm_IN);
+void decimalToBinary(int num, int *binImmed, int bits);
 void parseSBType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int Imm_IN);
 int regIndex(char *reg);
