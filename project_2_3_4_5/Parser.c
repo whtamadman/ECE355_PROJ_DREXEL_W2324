c400894806bffa916bed578cc9eb654076d3f909
whtamadman <81600724+whtamadman@users.noreply.github.com>
2024-02-14 20:28:59 -0500

    Connected Project 1 to 2-5

--- project_2_3_4_5/Parser.c
+++ project_2_3_4_5/Parser.c
@@ -2,6 +2,21 @@
 
 // FIXME, implement this function.
 // Here shows an example on how to translate "add x10, x10, x25"
+
+char *Rtype[] = {"add","sub","sll","slt","sltu","xor","srl","sra","or","and","addw","subw","sllw","srlw","sraw"};
+int RtypeOpcode[] = {51,51,51,51,51,51,51,51,51,51,59,59,59,59,59};
+int Rtype_func3[] = {0,0,1,2,3,4,5,5,6,7,0,0,1,5,5};
+int Rtype_func7[] = {0,32,0,0,0,0,0,32,0,0,0,32,0,0,32};
+
+char *Itype[] = {"lb","lh","lw","ld","lbu","lhu","lwu","fence","fence.i","addi","slli","slti","sltiu","xori","srli","srai","ori","andi","addiw","slliw","srliw","sraiw"};
+int ItypeOpcode[] = {3,3,3,3,3,3,3,15,15,19,19,19,19,19,19,19,19,19,27,27,27,27};
+int Itype_func3[] = {0,1,2,3,4,5,6,0,1,0,1,2,3,4,5,5,6,7,0,1,5,5};
+int Itype_imm[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,0,0,0,0,0,32};
+
+char *SBtype[] = {"beq","bne","blt","bge","bltu","bgeu"};
+int SBtypeOpcode[] = {99,99,99,99,99,99};
+int SBfunc3[] = {0,1,4,5,6,7};
+
 void loadInstructions(Instruction_Memory *i_mem, const char *trace)
 {
     printf("Loading trace file: %s\n", trace);
@@ -27,6 +42,32 @@ void loadInstructions(Instruction_Memory *i_mem, const char *trace)
 
         // Extract operation
         char *raw_instr = strtok(line, " ");
+
+        //For R-Types
+        for (int i = 0; i < 15; i++) {
+            if (strcmp(raw_instr,Rtype[i]) == 0) {
+                // printf("Function:%s\n\tOpcode:%d\n\tFunc3:%d\n\tFunc7:%d\n",Rtype[i], RtypeOpcode[i], Rtype_func3[i], Rtype_func7[i]);
+                parseRType(raw_instr, &(i_mem->instructions[IMEM_index]), RtypeOpcode[i], Rtype_func3[i], Rtype_func7[i]);
+                i_mem->last = &(i_mem->instructions[IMEM_index]);
+            }
+        }
+        //For I-Types
+        for (int i = 0; i < 22; i++) {
+            if (strcmp(raw_instr, Itype[i]) == 0) {
+                // printf("Function:%s\n\tOpcode:%d\n\tFunc3:%d\n\tImm:%d\n", Itype[i], ItypeOpcode[i], Itype_func3[i], Itype_imm[i]);
+                parseIType(raw_instr, &(i_mem->instructions[IMEM_index]), ItypeOpcode[i], Itype_func3[i], Itype_imm[i]);
+                i_mem->last = &(i_mem->instructions[IMEM_index]);
+            }
+        }
+        //For SB-Types
+        for (int i = 0; i < 6; i++) {
+            if (strcmp(raw_instr, SBtype[i]) == 0) {
+                // printf("Function:%s\n\tOpcode:%d\n\tFunc3:%d\n\tImm:%d\n", SBtype[i], SBtypeOpcode[i], SBfunc3[i], SBtype_imm[i]);
+                parseSBType(raw_instr, &(i_mem->instructions[IMEM_index]), SBtypeOpcode[i], SBfunc3[i]);
+                i_mem->last = &(i_mem->instructions[IMEM_index]);
+            }
+        }
+/*      char *raw_instr = strtok(line, " ");
         if (strcmp(raw_instr, "add") == 0 ||
             strcmp(raw_instr, "sub") == 0 ||
             strcmp(raw_instr, "sll") == 0 ||
@@ -37,7 +78,7 @@ void loadInstructions(Instruction_Memory *i_mem, const char *trace)
         {
             parseRType(raw_instr, &(i_mem->instructions[IMEM_index]));
             i_mem->last = &(i_mem->instructions[IMEM_index]);
-	}
+	} */
 
         IMEM_index++;
         PC += 4;
@@ -46,19 +87,12 @@ void loadInstructions(Instruction_Memory *i_mem, const char *trace)
     fclose(fd);
 }
 
-void parseRType(char *opr, Instruction *instr)
+void parseRType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int funct7_IN)
 {
     instr->instruction = 0;
-    unsigned opcode = 0;
-    unsigned funct3 = 0;
-    unsigned funct7 = 0;
-
-    if (strcmp(opr, "add") == 0)
-    {
-        opcode = 51;
-        funct3 = 0;
-        funct7 = 0;
-    }
+    unsigned opcode = opcode_IN;
+    unsigned funct3 = funct3_IN;
+    unsigned funct7 = funct7_IN;
 
     char *reg = strtok(NULL, ", ");
     unsigned rd = regIndex(reg);
@@ -67,6 +101,7 @@ void parseRType(char *opr, Instruction *instr)
     unsigned rs_1 = regIndex(reg);
 
     reg = strtok(NULL, ", ");
+    
     reg[strlen(reg)-1] = '\0';
     unsigned rs_2 = regIndex(reg);
 
@@ -78,6 +113,109 @@ void parseRType(char *opr, Instruction *instr)
     instr->instruction |= (rs_2 << (7 + 5 + 3 + 5));
     instr->instruction |= (funct7 << (7 + 5 + 3 + 5 + 5));
 }
+
+void parseIType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN, int Imm_IN)
+{
+    instr->instruction = 0;
+    unsigned opcode = opcode_IN;
+    unsigned funct3 = funct3_IN;
+    unsigned Imm = Imm_IN;
+    int load = 0;
+    unsigned rd;
+    unsigned rs_1;
+
+    //Check if Func is Load
+    for (int i = 0; i < 8; i++) {
+        if (strcmp(opr, Itype[i]) == 0) {
+            load = 1;
+        }
+    }
+    
+    //Universal
+    char *reg = strtok(NULL, ", ");
+    rd = regIndex(reg);
+    //Non-Load 
+    if (load == 0) {
+        reg = strtok(NULL, ", ");
+        rs_1 = regIndex(reg);
+        reg = strtok(NULL, ", ");
+        Imm += atoi(reg);
+        reg[strlen(reg)-1] = '\0';
+        // printf("%d",atoi(reg));
+    }
+    //Load:
+    else if (load == 1) {
+        reg = strtok(NULL, "(");
+        Imm += atoi(reg);
+        // printf("Imm: %d\n",atoi(reg));
+        reg = strtok(NULL, ")");
+        rs_1 = regIndex(reg);
+        reg[strlen(reg)-1] = '\0';
+        // printf("rs_1= %d\n",rs_1);
+    }
+
+    // Contruct instruction
+    instr->instruction |= opcode;
+    instr->instruction |= (rd << 7);
+    instr->instruction |= (funct3 << (7 + 5));
+    instr->instruction |= (rs_1 << (7 + 5 + 3));
+    instr->instruction |= (Imm << (7 + 5 + 3 + 5));
+}
+
+void decimalToBinary(int num, int *binImmed, int bits)
+{
+    num = num/2;
+    for (int i = bits - 1; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        binImmed[i] = bit;
+    }
+}
+
+void parseSBType(char *opr, Instruction *instr, int opcode_IN, int funct3_IN)
+{
+    instr->instruction = 0;
+    unsigned opcode = opcode_IN;
+    unsigned funct3 = funct3_IN;
+    unsigned Imm = 0;
+    int ImmBin[12];
+    unsigned rd;
+    unsigned rs_1;
+    unsigned rs_2;
+
+    char *reg = strtok(NULL, ", ");
+    rs_1 = regIndex(reg);
+    reg = strtok(NULL, ", ");
+    rs_2 = regIndex(reg);
+    reg = strtok(NULL, ", ");
+    Imm += atoi(reg);
+    //printf("%d\n", Imm);
+    reg[strlen(reg)-1] = '\0';
+
+    decimalToBinary(Imm, ImmBin, 12);
+    // for (int i=0; i<12; i++) {
+    //     printf("%d", ImmBin[i]);
+    // }
+    // printf("\n");
+
+    // Contruct instruction
+    instr->instruction |= opcode;
+    instr->instruction |= (ImmBin[10] << 7);
+    instr->instruction |= (ImmBin[0] << (7 + 1));
+    instr->instruction |= (ImmBin[1] << (7 + 2));
+    instr->instruction |= (ImmBin[2] << (7 + 3));
+    instr->instruction |= (ImmBin[3] << (7 + 4));
+    instr->instruction |= (funct3 << (7 + 5));
+    instr->instruction |= (rs_1 << (7 + 5 + 3));
+    instr->instruction |= (rs_2 << (7 + 5 + 3 + 5));
+    instr->instruction |= (ImmBin[4] << (7 + 5 + 3 + 5 + 5));
+    instr->instruction |= (ImmBin[5] << (7 + 5 + 3 + 5 + 5 + 1));
+    instr->instruction |= (ImmBin[6] << (7 + 5 + 3 + 5 + 5 + 2));
+    instr->instruction |= (ImmBin[7] << (7 + 5 + 3 + 5 + 5 + 3));
+    instr->instruction |= (ImmBin[8] << (7 + 5 + 3 + 5 + 5 + 4));
+    instr->instruction |= (ImmBin[9] << (7 + 5 + 3 + 5 + 5 + 5));
+    instr->instruction |= (ImmBin[11] << (7 + 5 + 3 + 5 + 5 + 6));
+}
+
 
 int regIndex(char *reg)
 {
